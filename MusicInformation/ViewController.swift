41b8f406e94b3a63e35ea326023bd8f423746954
Andrea Baldelomar <Anbade@Andreas-Mac.local>
2016-01-06 02:05:08 -0400

    add avaudioplayer view

--- MusicInformation/ViewController.swift
+++ MusicInformation/ViewController.swift
@@ -8,28 +8,28 @@
 
 import UIKit
 import AVFoundation
+import AVKit
 
 class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVAudioPlayerDelegate {
     
     //View Controllers
-    @IBOutlet weak var texFieldMusic: UITextField!
     @IBOutlet weak var labelMusic: UILabel!
     @IBOutlet weak var imageTortoise: UIImageView!
     
     var track = Track?()
     var audioPlayer = AVAudioPlayer()
     var urlSong = String()
+    var image = UIImage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        texFieldMusic.delegate = self
         // Set up views if editing an existing Meal.
         if let track = track {
             navigationItem.title = track.artistName
-            texFieldMusic.text   = track.artistName
-            imageTortoise.image = track.image
+           imageTortoise.image = track.image
             labelMusic.text = track.song
             urlSong = track.urlSong
+            image = track.image!
         }
     }
 
@@ -40,8 +40,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     //Actions
     @IBAction func buttonSearch(sender: UIButton) {
-        labelMusic.text = "Music Name"
-        do{
+        /*do{
             let url = urlSong
             let fileURL = NSURL(string:url)
             let soundData = NSData(contentsOfURL:fileURL!)
@@ -52,22 +51,11 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
             audioPlayer.play()
         }catch {
             print("Error getting the audio file")
-        }
-    }
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        //hide keyboard
-        texFieldMusic.resignFirstResponder()
-        return true
-    }
-    
-    func textFieldDidEndEditing(textField: UITextField) {
-        labelMusic.text = texFieldMusic.text
+        }*/
     }
     
     
     @IBAction func selectFromGallery(sender: UITapGestureRecognizer) {
-        texFieldMusic.resignFirstResponder()
         let imagePickerController = UIImagePickerController()
         imagePickerController.sourceType = .PhotoLibrary
         imagePickerController.delegate = self
@@ -75,17 +63,12 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         presentViewController(imagePickerController,animated:true, completion:nil)
     }
     
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        print("algoooooooooo")
-        dismissViewControllerAnimated(true, completion:nil)
-    }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        
-        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        imageTortoise.image = selectedImage
-        //dissmiis controller image
-        dismissViewControllerAnimated(true, completion: nil)
+    override func prepareForSegue(segue: UIStoryboardSegue,
+        sender: AnyObject?) {
+            let destination = segue.destinationViewController as!
+            AVPlayerViewController
+            let url = NSURL(string: urlSong)!
+            destination.player = AVPlayer(URL: url)
     }
     
 }
